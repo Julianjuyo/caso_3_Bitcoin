c81a91f738c8e5b14a3dfa3bdf2242ae9236fd3c
Julian Oliveros Forero <60152513+Julianjuyo@users.noreply.github.com>
2021-04-27 17:22:35 -0500

    Update Main.java
    cambios en el main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n"+"Por favor ingrese el algoritmo que desea utilizar (SHA-512 o SHA-256), la cantidad de ceros que desea que tenga al comienzo el hash que se buscará y la cadena inicial con la que se hará la prueba");
+        System.out.println("\n"+"Por favor ingrese el algoritmo que desea utilizar (SHA-512 o SHA-256), la cantidad de ceros que desea que tenga al comienzo el hash que se buscará y la cadena inicial con la que se hará la prueba on maximo 32 caracteres");
         System.out.println("Los datos deben ser ingresados separados por comas");
         System.out.println("Por ejemplo: SHA-512,24,esta es la cadena inicial");
         InputStreamReader is= new InputStreamReader(System.in);
@@ -18,15 +18,22 @@ public class Main {
             String algoritmo = split[0];
             int cantidadDeCeros = Integer.parseInt(split[1]);
             String cadena = split[2];
+            
+            if(!(algoritmo.equals("SHA-256")||algoritmo.equals("SHA-512"))){
+                throw new Exception("El algoritmo ingresado no es válido");
+            }
+            
+            if(cadena.length()>32){
+                throw new Exception("Error la cadena tiene mas de 32 caracteres");
+            }
 
 
             long startTime = System.currentTimeMillis();
             String v = new Buscador(new Hash(algoritmo)).buscarCombinacionParaQueElHashCumplaLaCondicion(cantidadDeCeros, cadena);
             long endTime = System.currentTimeMillis() - startTime;
-            endTime= (endTime/60000);
             System.out.println("La cadena (v) encontrada es: " + v);
             System.out.println("La cadena usada es: " + cadena);
-            System.out.println("Se tardó: "+ endTime +" minutos"+"\n");
+            System.out.println("Se tardó: "+ endTime +" milisegundos"+"\n");
 
 
         } catch (IOException e) {
