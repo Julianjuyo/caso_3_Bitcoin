c90e4c020092da7c2e4ef77af8925bac338f96ad
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2021-04-28 10:56:39 -0500

    cambios sistema operativo

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
     public static void escribirEnArchvio(int cantidadDeCeros, String cadena, BufferedWriter bw, String algoritmo){
         try {
+
+            bw.write("\n" +"La cadena usada es:" + cadena + "\n");
+            System.out.println("\n" +"La cadena usada es:" + cadena + "\n");
+            bw.write("\nEl algoritmo usado es: " + algoritmo + "\n");
+            System.out.println("\nEl algoritmo usado es: " + algoritmo + "\n");
+
             long startTime = System.currentTimeMillis();
             System.out.println("Prueba con "+cantidadDeCeros+" ceros");
             bw.write("\n"+"Prueba con "+cantidadDeCeros+" ceros"+"\n");
             String v = new Buscador(new Hash(algoritmo), bw).buscarCombinacionParaQueElHashCumplaLaCondicion(cantidadDeCeros, cadena);
             long endTime = System.currentTimeMillis() - startTime;
             System.out.println("La cadena (v) encontrada es: " + v);
-            System.out.println("Se tardó: "+ endTime +" minutos"+"\n");
+            System.out.println("Se tardó: "+ endTime +" milisegundos"+"\n");
             bw.write("La cadena (v) encontrada es: " + v + "\n");
             bw.write("Se tardó: "+ endTime +" milisegundos"+"\n");
             System.out.println("------------------------------------------------------------------------");
@@ -36,39 +42,30 @@ public class Main {
             String algoritmo = split[0];
             String cadena = split[1];
 
-            if(!(algoritmo.equals("SHA-256")||algoritmo.equals("SHA-512"))){
+            //Se verifica que los datos ingrsados cumplan con las condiciones
+            if(!(algoritmo.equals("SHA-256")||algoritmo.equals("SHA-512")))
                 throw new Exception("El algoritmo ingresado no es válido");
-            }
 
-            if(cadena.length()>32){
+            if(cadena.length()>32)
                 throw new Exception("Error la cadena tiene mas de 32 caracteres");
+
+            String pathNuevoArchvio;
+            String sSistemaOperativo = System.getProperty("os.name");
+
+            for (int i = 20; i <=36 ; i=i+4) {
+
+                //Se verifica el sistema operativo donde se corre
+                if(sSistemaOperativo.equals("Mac OS X"))
+                    pathNuevoArchvio ="/Users/julianoliveros/Desktop/Pruebas-"+i+"-"+algoritmo+"-conCadena-"+cadena+".txt";
+                else
+                    pathNuevoArchvio ="home/ubuntu/caso_3_Bitcoin/docs/Pruebas-"+i+"-"+algoritmo+"-conCadena-"+cadena+".txt";
+
+                BufferedWriter bw = new BufferedWriter(new FileWriter(pathNuevoArchvio));
+                escribirEnArchvio(i,  cadena,  bw,  algoritmo);
+                bw.close();
             }
-            String pathNuevoArchvio ="home/ubuntu/caso_3_Bitcoin/docs/Pruebas-"+algoritmo+"-conCadena-"+cadena+".txt";
-            //String pathNuevoArchvio ="/Users/julianoliveros/Desktop/Pruebas-"+algoritmo+"-conCadena-"+cadena+".txt";
-
-
-            BufferedWriter bw = new BufferedWriter(new FileWriter(pathNuevoArchvio));
-            bw.write("\n" +"La cadena usada es: " + cadena + "\n");
-            System.out.println("\n" +"La cadena usada es: " + cadena + "\n");
-            bw.write("\nEl algoritmo usado es: " + algoritmo + "\n");
-            System.out.println("\nEl algoritmo usado es: " + algoritmo + "\n");
-
-
-            escribirEnArchvio(20,  cadena,  bw,  algoritmo);
-
-            escribirEnArchvio(24,  cadena,  bw,  algoritmo);
-
-            escribirEnArchvio(28,  cadena,  bw,  algoritmo);
-
-            escribirEnArchvio(32,  cadena,  bw,  algoritmo);
-
-            escribirEnArchvio(36,  cadena,  bw,  algoritmo);
-
-            bw.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
